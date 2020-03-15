49dfc801c1ce735999ff58a8f921667c443a9448
modem-man <modem-man@d56bd52f-c789-4a0b-94f5-533848189669>
2020-03-15 18:00:38 +0000

    Added a sample for very easy palin-C-compilation

---
+++ trunk/samples.c/helloworld.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* Compilation & linking:
+ *      gcc -Os helloworld.c -o helloworld
+ *      strip helloworld  [.exe on MSYS/Cygwin/...]
+ *
+ * execution:
+ *      ./helloworld -?
+ *      ./helloworld
+ *
+ * or Compilation & linking with Debug support:
+ *      gcc -ggdb helloworld.c -o helloworld
+ *
+ * debugging:
+ *      gdb -ex=l --args ./helloworld -x
+ */
+
+int main( int argc, const char *argv[] )
+{
+	if( argc>1 && (argv[1][0]=='-' || argv[1][0]=='/') )
+	{
+		switch( argv[1][1] )
+		{
+			case 'h' :
+			case 'H' :
+			case '?' :
+				fprintf(stderr, "This program sends greetings to the world\n" );
+				break;
+		}
+	}
+	else
+	{
+		puts( "Hello World!\n" );
+	}
+	return 0;
+}
