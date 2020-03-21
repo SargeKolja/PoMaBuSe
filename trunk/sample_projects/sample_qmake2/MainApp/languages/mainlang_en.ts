676ec8ec23bc88bcfb71854e04473e726cd42813
modem-man <modem-man@d56bd52f-c789-4a0b-94f5-533848189669>
2020-03-21 23:57:10 +0000

    refreshed sample translation files, unimportant

--- trunk/sample_projects/sample_qmake2/MainApp/languages/mainlang_en.ts
+++ trunk/sample_projects/sample_qmake2/MainApp/languages/mainlang_en.ts
@@ -1,27 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
-<defaultcodec>UTF-8</defaultcodec>
+<TS version="2.1" language="en_US">
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../Sub1.cpp" line="18"/>
         <source>Give me a Number</source>
         <comment>just some text without any meaning</comment>
-        <translation>Give me a Number</translation>
+        <translation type="vanished">Give me a Number</translation>
     </message>
     <message>
-        <location filename="../Sub1.cpp" line="22"/>
         <source>Hello %1
 </source>
         <comment>%1 gets replaced with a noun (&apos;world&apos;,&apos;moon&apos;,...) at runtime</comment>
-        <translation>Hello %1\n</translation>
+        <translation type="vanished">Hello %1\n</translation>
     </message>
     <message>
-        <location filename="../Sub1.cpp" line="23"/>
         <source>world</source>
         <comment>the noun</comment>
-        <translation>world</translation>
+        <translation type="vanished">world</translation>
     </message>
 </context>
 </TS>
