6903398569e09d48df2c4b43dec99d2ae83b0ee2
modem-man <modem-man@gmx.net>
2020-03-31 12:41:41 +0000

    option (default=on) sending ANSI colorized ERROR/WARN/OKAY to stdout console option (default=on) sending HTML colorized ERROR/WARN/OKAY report attachment option (default=on) sending HTML as report attachment

--- PoMaBuSe_PoorMansBuildServer.sh
+++ PoMaBuSe_PoorMansBuildServer.sh
@@ -83,7 +83,7 @@ while [ ! -f ${STOP_FLAG} ]; do
 		LAST_TRIED_REV=-1
 		if [ ! -r "${REVISION_FILE}" ]; then
 			echo "- 1st time init ${REVISION_FILE} to LAST_GOOD_REV=${LAST_GOOD_REV} LAST_TRIED_REV=${LAST_TRIED_REV}" | tee -a "${COMPILER_LOGFILE}"
-			tools_update_used_versions "${REVISION_FILE}" ${LAST_GOOD_REV} ${LAST_TRIED_REV} 2>&1 | tee -a "${COMPILER_LOGFILE}"  # setting last tried below last good forced 1st time compilation
+			tools_update_used_versions "${REVISION_FILE}" ${LAST_GOOD_REV} ${LAST_TRIED_REV} 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"  # setting last tried below last good forced 1st time compilation
 		fi
 		if [ -r "${REVISION_FILE}" ]; then
 			source "${REVISION_FILE}"
@@ -95,7 +95,7 @@ while [ ! -f ${STOP_FLAG} ]; do
 		# -----
 		tools_dump_jobdetails "${job}" | tee -a "${COMPILER_LOGFILE}"
 		if [ "OKAY" != "$(CHECK_${REPORT_ENGINE})" ]; then
-			echo "missing report engine, can't report this" | tee -a "${COMPILER_LOGFILE}"
+			echo "Warning: missing report engine, can't report this" | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 			CHECK_${REPORT_ENGINE} 2>&1  | tee -a "${COMPILER_LOGFILE}"
 			# stderr_report_engine $? "FAILED" "${FromMail}" "${ProjectName}" "1" "${COMPILER_LOGFILE}" "${FromMail}"
 			report_error $? "${ProjectName}" "missing report engine ${REPORT_ENGINE}"
@@ -104,27 +104,27 @@ while [ ! -f ${STOP_FLAG} ]; do
 		echo "checking if ${SANDBOX} is halfway working ..."
 		if [ ! -r ${SANDBOX} ]; then
 			echo "getting ${URL} / ${VCSTRUNK} revision ${REVISION}:" | tee -a "${COMPILER_LOGFILE}"
-			${VCS_CHECKOUT} "${SANDBOX}" 2>&1 | tee -a "${COMPILER_LOGFILE}"
+			${VCS_CHECKOUT} "${SANDBOX}" 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 		fi
 		if [ -r ${SANDBOX} ]; then
 			echo "checking status ..." | tee -a "${COMPILER_LOGFILE}"
 			# LastEditors=$(${SCAN_AUTHORS} "${SANDBOX}" "${LAST_GOOD_REV}" "${TRY_THIS_REV}")
 			# echo "LastEditors=${LastEditors}"
 			if [ "YES" == $(${CHECK_SANDBOX} "${SANDBOX}") ]; then
-				echo "- is a ${VSCNAME} sandbox, ok" | tee -a "${COMPILER_LOGFILE}"
+				echo "- is a ${VSCNAME} sandbox, okay" | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 				if [ "YES" == $(${IF_CHANGED} "${SANDBOX}") \
 				     -o ${LAST_GOOD_REV} -lt ${LAST_TRIED_REV} \
 					 -o ${LAST_GOOD_REV} -le 0 \
 					 -o ${LAST_TRIED_REV} -le 0 ]; then
 					echo "- is more recent on the server, need to dive in ..." | tee -a "${COMPILER_LOGFILE}"
 					echo "invoking ${VCS_UPDATE} ${SANDBOX}" | tee -a "${COMPILER_LOGFILE}"
-					${VCS_UPDATE} "${SANDBOX}" 2>&1 | tee -a "${COMPILER_LOGFILE}"
+					${VCS_UPDATE} "${SANDBOX}" 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 					if [ 0 -eq ${PIPESTATUS[0]} -a "NO" == $(${IF_CHANGED} "${SANDBOX}") ]; then
 						echo "- updated successfully, can BUILD now" | tee -a "${COMPILER_LOGFILE}"
 						TRY_THIS_REV=$(${GET_RELEASEID} "${SANDBOX}")
 						if [ ${TRY_THIS_REV} -gt ${LAST_TRIED_REV} ]; then
 							echo "+ ${TRY_THIS_REV} newer than ${LAST_TRIED_REV}, so we can give it a new try" | tee -a "${COMPILER_LOGFILE}"
-							tools_update_used_versions "${REVISION_FILE}" "${LAST_GOOD_REV}" "${LAST_TRIED_REV}" 2>&1 | tee -a "${COMPILER_LOGFILE}"
+							tools_update_used_versions "${REVISION_FILE}" "${LAST_GOOD_REV}" "${LAST_TRIED_REV}" 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 							LastEditors=$(${SCAN_AUTHORS} "${SANDBOX}" "${LAST_GOOD_REV}" "${TRY_THIS_REV}")
 							echo -e "> LastEditors have been:\n\t${LastEditors}" | tee -a "${COMPILER_LOGFILE}"
 							# compile ...
@@ -136,31 +136,46 @@ while [ ! -f ${STOP_FLAG} ]; do
 							DO_BUILD_AND_REPORT=${COMPILE}${DO_REPORT}
 							echo -e "# calling compiler:\n\t${DO_BUILD_AND_REPORT}" | tee -a "${COMPILER_LOGFILE}"
 							date --iso-8601=ns | tee -a "${COMPILER_LOGFILE}"
-							( eval "${DO_BUILD_AND_REPORT}" ) 2>&1 | tee -a "${COMPILER_LOGFILE}"
+							# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+							# here we compile!
+							# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+							( eval "${DO_BUILD_AND_REPORT}" ) 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 							Err=$?
 							Res="$(tail -1 < "${COMPILER_LOGFILE}")"
+							# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 							# echo "Err=$Err, Res=$Res, Res:0:9=${Res:0:9}, Res:10=${Res:10}"
 							if [ ${Err} -eq 0 -a "---FAIL---" == ${Res:0:10} ]; then
 								let Err=${Res:10}
-								echo "imported error code Err=$Err"
+								echo "imported error code Err=$Err" | ColorizeANSI
 							fi
 							# does not work here!? -- echo "PIPESTATUS[*]=${PIPESTATUS[*]}"
 							echo "compiler/script returned ${Err}" | tee -a "${COMPILER_LOGFILE}"
 							date --iso-8601=ns | tee -a "${COMPILER_LOGFILE}"
+							AttachedLogfile="${COMPILER_LOGFILE}"
 							if [ ${Err} -eq 0 ]; then
 								cd ${PoMaBuSeDir}
-								echo "+ compilation of version ${TRY_THIS_REV} was okay" | tee -a "${COMPILER_LOGFILE}"
+								echo "+ compilation of version ${TRY_THIS_REV} was okay" | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 								# todo: parameter or switch to decide if POSITIVE reports shall be sent (or sent after recovered failure to failure recipients?)
-								${REPORT_ENGINE} ${Err} "SUCCESS" "${FromMail}" "${ProjectName}" "${TRY_THIS_REV}" "${COMPILER_LOGFILE}"  ${LastEditors}
+								if ${UseAnsiColorFilter}; then ColorwashANSI_file  "${COMPILER_LOGFILE}"; fi
+								if ${UseHTMLlogfile}; then
+									ToColorHTMLDOC_file "${COMPILER_LOGFILE}" "${COMPILER_LOGFILE}.html"
+									AttachedLogfile="${COMPILER_LOGFILE}.html"
+								fi
+								${REPORT_ENGINE} ${Err} "SUCCESS" "${FromMail}" "${ProjectName}" "${TRY_THIS_REV}" "${AttachedLogfile}"  ${LastEditors}
 								# setting both revisions equal on success:
-								tools_update_used_versions "${REVISION_FILE}" "${TRY_THIS_REV}" "${TRY_THIS_REV}" 2>&1 | tee -a "${COMPILER_LOGFILE}"
+								tools_update_used_versions "${REVISION_FILE}" "${TRY_THIS_REV}" "${TRY_THIS_REV}" 2>&1 | ColorizeANSI | tee -a "${COMPILER_LOGFILE}"
 							else
 								cd "${PoMaBuSeDir}"
 								echo "--- Changes since last successful compilation:" | tee -a "${COMPILER_LOGFILE}"
 								${GET_LAST_CHANGES} "${SANDBOX}" "${COMPILER_LOGFILE}" "${LAST_GOOD_REV}" "${TRY_THIS_REV}"
 								echo "reporting ERR to ${REPORT_ENGINE}" | tee -a "${COMPILER_LOGFILE}"
-								${REPORT_ENGINE} ${Err} "FAILED" "${FromMail}" "${ProjectName}" "${TRY_THIS_REV}" "${COMPILER_LOGFILE}"  ${LastEditors}
-								echo "compilation FAILED"
+								if ${UseAnsiColorFilter}; then ColorwashANSI_file  "${COMPILER_LOGFILE}"; fi
+								if ${UseHTMLlogfile}; then
+									ToColorHTMLDOC_file "${COMPILER_LOGFILE}" "${COMPILER_LOGFILE}.html"
+									AttachedLogfile="${COMPILER_LOGFILE}.html"
+								fi
+								${REPORT_ENGINE} ${Err} "FAILED" "${FromMail}" "${ProjectName}" "${TRY_THIS_REV}" "${AttachedLogfile}" ${LastEditors}
+								echo "compilation FAILED" | ColorizeANSI
 							fi
 						else
 							echo "? already tried to build version ${LAST_TRIED_REV}, skipping this"
